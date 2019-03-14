45cd3e9e640b0283e0705d61e603bd959ac1bbe5
Steve J. Hodges <sthodges@cabrillo.edu>
2019-03-13 17:24:37 -0700

    version after Wednesday 03/13/19 class
    added player movement, physics, collision, jumping, basic animation, animation controller, some moving platform code, TODO: moving platofrm gizmo and first moving platforms

---
+++ Assets/Sprites/MovingPlatform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+	public Vector3 finishPos = Vector3.zero;
+	public float speed = 0.5f;
+
+	private Vector3 _startPos;
+	private float _trackPercent = 0.0f;
+	private int _direction = 1;
+
+
+	// Use this for initialization
+	void Start () {
+		_startPos = transform.position; 	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_trackPercent += _direction * speed * Time.deltaTime;
+		float x = (finishPos.x - _startPos.x) * _trackPercent + _startPos.x;
+		float y = (finishPos.y - _startPos.y) * _trackPercent + _startPos.y;
+		transform.position = new Vector3 (x, y, _startPos.z);
+		if ((_direction == 1 && _trackPercent > 0.9f) ||
+		    (_direction == -1 && _trackPercent < 0.1f)) {
+
+			_direction = -_direction;
+		}
+	}
+}
---
+++ Assets/Sprites/PlatformerPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerPlayer : MonoBehaviour {
+	public float speed = 250.0f;
+	private Rigidbody2D _body;
+	private Animator _anim;
+	private BoxCollider2D _box;
+	public float jumpforce = 12.0f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		_body = GetComponent<Rigidbody2D> ();
+		_anim = GetComponent<Animator> ();
+		_box = GetComponent<BoxCollider2D> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float deltaX = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+		Vector2 movement = new Vector2 (deltaX, _body.velocity.y);
+
+		_body.velocity = movement;
+
+		Vector3 max = _box.bounds.max;
+		Vector3 min = _box.bounds.min;
+
+		Vector2 corner1 = new Vector2 (max.x, min.y - 0.1f);
+		Vector2 corner2 = new Vector2 (min.x, min.y - 0.2f);
+		Collider2D hit = Physics2D.OverlapArea (corner1, corner2);
+		bool grounded = (hit != null);
+
+		_body.gravityScale = (grounded && deltaX == 0) ? 0 : 1;
+
+
+		if (grounded && Input.GetKeyDown (KeyCode.Space)) {
+			_body.AddForce (Vector2.up * jumpforce, ForceMode2D.Impulse);
+		}
+
+
+		_anim.SetFloat ("speed", Mathf.Abs (deltaX));
+
+		if (!Mathf.Approximately (deltaX, 0)) {
+
+			transform.localScale = new Vector3 (Mathf.Sign (deltaX), 1, 1);
+		}
+
+	}
+}
